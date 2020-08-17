3da22d55d17a774a696c43837bb8e4ab9a86ac54
codemePrajna <prajna.hegde5891@gmail.com>
2020-08-18 00:12:44 +0530

    service layer unit test

--- src/test/java/com/myRetail/ProductServiceTest.java
+++ src/test/java/com/myRetail/ProductServiceTest.java
@@ -7,12 +7,19 @@ import com.myRetail.response.Response;
 import com.myRetail.service.impl.ProductFetchServiceImpl;
 import com.myRetail.service.impl.ProductUpdateServiceImpl;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventhandling.DomainEventMessage;
+import org.axonframework.eventhandling.GenericDomainEventMessage;
+import org.axonframework.eventsourcing.eventstore.DomainEventStream;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.messaging.Message;
+import org.axonframework.messaging.MetaData;
 import org.axonframework.messaging.responsetypes.ResponseType;
 import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.test.saga.FixtureConfiguration;
 import org.codehaus.jettison.json.JSONException;
+
+import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,6 +28,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -40,6 +48,9 @@ public class ProductServiceTest {
     @Mock
     FixtureConfiguration fixture;
 
+    @Mock
+    EventStore eventStore;
+
 
     private String productId;
     private Product product = new Product();
@@ -50,7 +61,7 @@ public class ProductServiceTest {
     @Before
     public void setUp() {
         // fixture = new AggregateTestFixture<>(CommandGateway.class);
-
+        MockitoAnnotations.initMocks(this);
         productId = "12345";
         product.setProductId(productId);
         product.setPrice(15.0);
@@ -65,9 +76,14 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void product_fetch_test() throws ProductException, InterruptedException, ExecutionException, JSONException, ProductException {
+    public void product_create_test_when_data_exists() throws ProductException, InterruptedException, ExecutionException, JSONException, ProductException {
         CommandGateway commandGateway = Mockito.mock(CommandGateway.class);
-        EventStore eventStore = Mockito.mock(EventStore.class);
+        DomainEventMessage event1 = new GenericDomainEventMessage<>("type", UUID.randomUUID().toString(), (long) 0,
+                "Mock contents", MetaData.emptyInstance());
+        DomainEventMessage event2 = new GenericDomainEventMessage<>("type", UUID.randomUUID().toString(), (long) 0,
+                "Mock contents", MetaData.emptyInstance());
+        DomainEventStream eventStream = DomainEventStream.of(event1, event2);
+        //EventStore eventStore = Mockito.mock(EventStore.class);
         fixture.registerResource(commandGateway);
         fixture.registerResource(eventStore);
         productUpdateService = new ProductUpdateServiceImpl(commandGateway,eventStore);
@@ -81,15 +97,57 @@ public class ProductServiceTest {
         CompletableFuture<Object> productIdCf = Mockito.mock(CompletableFuture.class);
         Mockito.when(productIdCf.get()).thenReturn(productId);
         Mockito.when(commandGateway.send(Mockito.any())).thenReturn(productIdCf);
-        Mockito.when(eventStore.readEvents(productId)
-                .asStream()
-                .map(Message::getPayload)
-                .collect(Collectors.toList()).size()).thenReturn(Integer.valueOf("0"));
+        Mockito.when(eventStore.readEvents(productId)).thenReturn(eventStream);
 
         CompletableFuture<Object> productCf = Mockito.mock(CompletableFuture.class);
         Mockito.when(productCf.get()).thenReturn(product);
         Mockito.when(queryGateway.query(Mockito.any(), Mockito.any(ResponseType.class))).thenReturn(productCf);
-        productUpdateService.createProductDetails(productDto);
+        try {
+            productUpdateService.createProductDetails(productDto);
+            Assert.fail("Should have thrown an exception");
+        } catch (ProductException e) {
+
+            Assert.assertEquals(String.format("Product 12345 already exists"), e.getMessage());
+        }
+
+       /* ProductDto productDtoResponse = productFetchService.findByProductId(productId);
+        Assert.assertEquals(productDto, productDtoResponse);*/
+
+    }
+
+    @Test
+    public void product_fetch_test_when_data_exists() throws ProductException, InterruptedException, ExecutionException, JSONException, ProductException {
+        CommandGateway commandGateway = Mockito.mock(CommandGateway.class);
+        //DomainEventMessage event1 = new GenericDomainEventMessage();
+        List<? extends DomainEventMessage<?>> testList = Mockito.mock(List.class);
+        DomainEventStream eventStream = DomainEventStream.of(testList);
+        //EventStore eventStore = Mockito.mock(EventStore.class);
+        fixture.registerResource(commandGateway);
+        fixture.registerResource(eventStore);
+        productUpdateService = new ProductUpdateServiceImpl(commandGateway,eventStore);
+
+        QueryGateway queryGateway = Mockito.mock(QueryGateway.class);
+
+        fixture.registerResource(queryGateway);
+        productFetchService = new ProductFetchServiceImpl(queryGateway, eventStore);
+        //fixture.registerComm
+        //CompletableFuture<Object> productIdCf = CompletableFuture.supplyAsync(() -> productId);
+        CompletableFuture<Object> productIdCf = Mockito.mock(CompletableFuture.class);
+        Mockito.when(productIdCf.get()).thenReturn(productId);
+        Mockito.when(commandGateway.send(Mockito.any())).thenReturn(productIdCf);
+        Mockito.when(eventStore.readEvents(productId)).thenReturn(eventStream);
+        /*Mockito.when(eventStore
+                .readEvents("34567")
+                .asStream()
+                .map(Message::getPayload)
+                .collect(Collectors.toList()).size()).thenReturn(0);*/
+        CompletableFuture<Object> productCf = Mockito.mock(CompletableFuture.class);
+        Mockito.when(productCf.get()).thenReturn(product);
+        Mockito.when(queryGateway.query(Mockito.any(), Mockito.any(ResponseType.class))).thenReturn(productCf);
+        productDto.setProductId(productId);
+            productUpdateService.createProductDetails(productDto);
+
+
         ProductDto productDtoResponse = productFetchService.findByProductId(productId);
         Assert.assertEquals(productDto, productDtoResponse);
 
